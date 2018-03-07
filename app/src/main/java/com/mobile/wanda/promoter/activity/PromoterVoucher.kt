01f79f0b3680b71e1b27234021ebfe408f11ab53
alexkombo <alex.kombo@radioafricagroup.co.ke>
2018-03-08 01:05:09 +0300

    creates promoter voucher management flow

--- app/src/main/java/com/mobile/wanda/promoter/activity/PromoterVoucher.kt
+++ app/src/main/java/com/mobile/wanda/promoter/activity/PromoterVoucher.kt
@@ -1,9 +1,52 @@
 package com.mobile.wanda.promoter.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.mobile.wanda.promoter.R
+import com.mobile.wanda.promoter.fragment.PromoterVoucherFragment
+import com.mobile.wanda.promoter.fragment.VoucherTopUpFragment
 
 /**
  * Created by kombo on 07/03/2018.
  */
-class PromoterVoucher: AppCompatActivity() {
+class PromoterVoucher : AppCompatActivity(), PromoterVoucherFragment.ClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.contentFrame, PromoterVoucherFragment())
+                    .commitAllowingStateLoss()
+        }
+    }
+
+    /**
+     * Find the promoter's id and send it as the parameter
+     */
+    override fun onRequestVoucherTopUp() {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.contentFrame, VoucherTopUpFragment.newInstance(1))
+                .commitAllowingStateLoss()
+    }
+
+    /**
+     * Handle hardware back button press
+     */
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> false
+        }
+    }
 }
